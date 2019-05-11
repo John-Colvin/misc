f138bd55fd9eb7d4a8309a726e69d6de16ea7070
John Colvin <john.loughran.colvin@gmail.com>
2019-05-11 17:03:19 +0100

    work with template lambdas

--- multiret.d
+++ multiret.d
@@ -1,38 +1,46 @@
-import std.meta : allSatisfy, staticMap;
-import std.traits : isCallable, Parameters;
-
-enum onlyOne(Args ...) = Args.length == 1;
+template OverloadSet(Args ...)
+{
+    static foreach (Arg; Args)
+        alias overloads = Arg;
+}
 
 struct multiRet(handlers ...)
-if (allSatisfy!(isCallable, typeof(handlers))
-    && allSatisfy!(onlyOne, staticMap!(Parameters, typeof(handlers))))
 {
     static struct HandlerGroup
     {
-        static foreach (handler; handlers)
-            static auto ref opCall()(auto ref Parameters!handler[0] v)
-            {
-                return handler(v);
-            }
+        static auto ref opCall(Args ...)(auto ref Args args)
+        {
+            return OverloadSet!(handlers).overloads(args);
+        }
     }
 
     static auto ref call(alias f, Args ...)(auto ref Args args)
     {
-		return f(HandlerGroup.init, args);
+        return f(HandlerGroup.init, args);
     }
 }
 
 unittest
 {
-	import std.stdio;
-	static struct Err { string msg; }
-	foreach (x; [-1, 1])
-		multiRet!(
-    			(int x) => x.writeln,
-    			(Err x) => x.msg.writeln)
-    		.call!((r, x) {
-        		if (x > 0)
-            		return r(x);
-				return r(Err("OH NO!"));
-    		})(x);
+    import std.stdio;
+    import std.conv;
+
+    static struct Err { string msg; }
+
+    alias foo = (r, x) {
+        if (x > 0)
+            return r(x);
+        else if (x < 0)
+            return r(Err("woops!"));
+        else
+            return r("OH SHIT");
+    };
+
+    foreach (x; [-1.0, 1.0, double.nan])
+        multiRet!(
+                (double x) => x.writeln,
+                (Err x) => x.msg.writeln,
+                (x) { throw new Exception(
+                    text("Got ", x, " what are you doing?!?")); })
+            .call!foo(x);
 }
